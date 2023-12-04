93d8453f5b32ca8524bc6daa7a95ec90525da334
Greg Schuman <gcschuman@hotmail.com>
2023-12-04 11:33:51 -0800

    Started working on the translator script.

---
+++ src/Components/Translator.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import "./Translator.css";
+import languageList from './Language.json';
+
+export default function Translator(){
+    const [inputFormat, setInputFormat] = useState('en');
+}
